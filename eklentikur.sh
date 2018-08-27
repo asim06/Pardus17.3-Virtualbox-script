956f16ce98fce4f3f87fb39f74fa2b7100e71553
Yunusemre Şentürk <yunusemre.senturk@pardus.org.tr>
2018-08-27 13:35:41 +0300

    Make the script more dynamic
    Signed-off-by: Yunusemre Şentürk <yunusemre.senturk@pardus.org.tr>

--- eklentikur.sh
+++ eklentikur.sh
@@ -2,8 +2,23 @@
 #Virtual box misafir eklentisi kurulumu
 #Virtual box Aygıtlar menüsünden Misafir Eklentileri CD Kalını yerleştir yapılmalı
 #Asım MISIRLI
-sudo apt install linux-headers-amd64 linux-headers-4.9.0-6-amd64
-sudo apt install build-essential module-assistant
-sudo cp -r  /media/cdrom0 /home
-sudo chmod 755 /home/cdrom0/VBoxLinuxAdditions.run
-sudo /home/cdrom0/./VBoxLinuxAdditions.run
+
+# author: yunusem
+# prepare for failsafe
+set -e
+# declare needed variables
+kernel=$(uname -r)
+mountpoint="/media/cdrom0"
+defaultDevicePoint="/dev/sr0"
+
+# mount existing cd
+sudo mount $defaultDevicePoint $mountpoint
+# update package manager cache
+sudo apt update
+# use dynamic kernel number incase it has been upgraded
+sudo apt install -y linux-headers-$kernel build-essential module-assistant
+# change directory to where media is mounted
+cd $mountpoint
+sudo /bin/bash VBoxLinuxAdditions.run
+# Show successfull finish
+echo "Bitti."
